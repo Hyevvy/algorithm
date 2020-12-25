d0c2e7336579373c01d20ad968f68ee0d5526bf2
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2020-12-25 13:52:34 +0900

    20201225

---
+++ 프로그래머스/JS/12912두정수사이의합.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+    var answer = 0;
+    let max = a > b ? a : b
+    let min = a < b ? a : b
+    for(let i=min; i<=max; i++){
+        answer += i ;
+    }
+    return answer;
+}
