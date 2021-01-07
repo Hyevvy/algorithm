f14951a46db439e91bdd4fbdba23652c62adaddb
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2021-01-08 00:20:54 +0900

    20210108

---
+++ BOJ/c++/boj15876BinaryCounting.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    int n, k;
+    cin>>n>>k;
+    vector<int> binaryV;
+    vector<int> currentBinary;
+    binaryV.push_back(0);
+    for(int i=0;binaryV.size() < n*5;i++){
+        for(int j=i; j>0; j=j/2){
+            currentBinary.push_back(j%2);
+        }
+        reverse(currentBinary.begin(),currentBinary.end());
+        binaryV.insert(binaryV.end(),currentBinary.begin(),currentBinary.end());
+        currentBinary.clear();
+    }
+   // cout<<binaryV.size()<<endl;
+    int count =0;
+    for(int i=0;i<binaryV.size() && count<5;i++){
+        if(i%n + 1 == k) {
+            cout<<binaryV[i]<<" ";
+            count++;}
+    }
+//    for(int i=0;i<binaryV.size();i++)
+//    cout<<binaryV[i];
+//    cout<<endl;
+    return 0;
+}
+
