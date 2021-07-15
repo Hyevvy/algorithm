d4cb96a2edab0d42ba991bec70d3d29adff297f5
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2021-07-16 02:32:39 +0900

    20210715(2)
    유클리드 ! 재귀 !

--- BOJ/c++/boj2609 최대공약수와 최소공배수.cpp
+++ BOJ/c++/boj2609 최대공약수와 최소공배수.cpp
@@ -2,54 +2,19 @@
 #include <cmath>
 #include <vector>
 using namespace std;
+int gcd(int a, int b){
+    if(a % b == 0) return b;
+    return gcd(b, a%b);
+}
 int main(){
     int a, b;
     cin >> a >> b;
-    int va[100] = {0,};
-    int vb[100] = {0,};
     
-    int i = a;
-    while(i > 1){
-        for(int j = 2; j < a; j++){
-            if( i % j == 0){
-                i = i / j;
-                va[j]++;
-                break;
-            }
-        }
-    }
+    int gcdNum = 0;
+    a > b ? gcdNum = gcd(a, b) : gcdNum = gcd(b, a);
     
-
+    cout << gcdNum<<"\n" << a * b / gcdNum << "\n";
     
-    i = b;
-    while(i > 1){
-        for(int j = 2; j < b; j++){
-            if( i % j == 0){
-                i = i / j;
-                vb[j]++;
-                break;
-            }
-        }
-    }
-    
-
-    //최대 공약수 확인
-    int GCD = 1; //최대공약수
-    int MCM = 1; //최소공배수
-    for(int i=1; i<100; i++){
-        if(va[i]!=0 && vb[i]!= 0)
-        {
-            GCD *= i;
-        }
-        if(va[i]!=0 || vb[i]!=0) {
-            va[i] > vb[i] ? MCM *= pow(i, va[i]) : MCM *= pow(i, vb[i]);
-        }
-    
-    }
-    
-    
-    cout<<GCD<<"\n";
-    cout<<MCM<<"\n";
     
     return 0;
 }
