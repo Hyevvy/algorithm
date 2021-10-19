5f86ef9e528da7bab1eecd97b10ed47d21cf3392
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2021-10-19 22:42:41 +0900

    Add files via upload

--- BOJ/c++/boj7785 회사에 있는 사람.cpp
+++ BOJ/c++/boj7785 회사에 있는 사람.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
+#include <set>
 #include <vector>
 #include <algorithm>
-#include <map>
 using namespace std;
 int main(){
-    map<string, int>mp; //name , 1: true 0:false
-    int tc;
-    cin>> tc;
+    int n;
+    cin >> n;
     string name, el;
-    vector<string> here;
-    for(int i=0; i<tc; i++){
+    set<string, greater<string>> st;
+    for(int i=0; i<n; i++){
         cin >> name >> el;
-        if(el=="enter") mp.insert({name, 1});
-        else mp.erase(name);
+        if(el=="enter") st.insert(name);
+        else st.erase(name);
     }
-    vector<string> v;
-    for(auto curr : mp)
-        if(curr.second == 1)
-            v.push_back(curr.first);
-    
-    reverse(v.begin(), v.end());
-    for(auto curr : v){
-        cout<< curr <<"\n";
+   
+    for(auto curr : st){
+        cout<< curr << "\n";
     }
-    
     return 0;
 }
-
