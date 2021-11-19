ba895adf9a49665416523062d367a3258568e8e3
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2021-11-19 10:38:22 +0900

    2021 11 19

---
+++ 프로그래머스/C++/12945 피보나치수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int dp[100010];
+int solution(int n) {
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] % 1234567 + dp[i - 2] % 1234567;
+    }
+
+
+    return dp[n] % 1234567;
+}
